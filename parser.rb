8e4e79f7ef7d736ee8188dc5d3f393eb70cb671d
Randy Hook <randallhook@gmail.com>
2013-07-17 22:07:13 -0400

    refactoring parser functions

--- parser.rb
+++ parser.rb
@@ -2,35 +2,81 @@ require_relative 'knowledge'
 
 class Parser
 
+	@@words = nil
+	
 	def Parser.parse(input)
 
-		words = input.split(' ')
-
+		@@words = input.split(' ')
 		wordCounter = 0
-		for word in words
-			case word
+		
+		for w in @@words
+			currentWord = Parser.getWord(wordCounter);
+
+			case currentWord.downcase
 				when 'is'
-					if (wordCounter < words.length - 1)
-						case words[wordCounter + 1]
-							when 'a', 'an'
-								if (wordCounter > 0 && wordCounter < words.length - 2)
-									puts 'You are saying that ' + words[wordCounter - 1] + ' is a type of ' + words[wordCounter + 2] + '?'
-									answer = gets.strip.downcase
-									
-									if (answer == 'y' || answer == 'yes') {
-										Knowledge.store(words[wordCounter - 1], 'is a', words[wordCounter + 2]);
-									}	
-									else {
-										puts 'OK, I will disregard that then'
-									}
-								end	
-						end	
-					end		
+					Parser.parseIs(wordCounter)
 			end
 
 			wordCounter += 1
 		end
 
+	end
+
+	def Parser.parseIs(wordPos)
+		if (wordPos < @@words.length - 1)
+			nextWord = getNextWords(wordPos + 1, 1)
+
+			case nextWord.join.downcase 
+				when 'a', 'an'
+					source = getPreviousWords(wordPos - 1, 1)
+					target = getNextWords(wordPos + 2, 1) 
+
+					unless (source.empty? || target.empty?)
+						puts 'You are saying that ' + source[0] + ' is a type of ' + target[0] + '?'
+						answer = gets.strip.downcase
+						
+						if (answer == 'y' || answer == 'yes')
+							Knowledge.store(source, 'is a', target);
+						else
+							puts 'OK, I will disregard that then'
+						end
+					else
+						puts 'Something went wrong'
+					end	
+			end	
+		end		
+	end
+
+	def Parser.getWord(wordPos)
+		return @@words[wordPos]
+	end
+
+	def Parser.getPreviousWords(startPos, wordCount)
+		snippet = Array.new
+		wordCounter = 0
+		currentWordPos = startPos
+
+		until wordCounter == wordCount || currentWordPos == -1
+			snippet.push(@@words[currentWordPos])
+			wordCounter += 1
+			currentWordPos -= 1
+		end
+		
+		return snippet
+	end
+
+	def Parser.getNextWords(startPos, wordCount)
+		snippet = Array.new
+		wordCounter = 0
+		currentWordPos = startPos
+		
+		until wordCounter == wordCount || currentWordPos >= @@words.length 
+			snippet.push(@@words[currentWordPos])
+			wordCounter += 1
+			currentWordPos += 1
+		end
+
+		return snippet
 	end
 
 end
